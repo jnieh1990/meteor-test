247279ab05cdaa176f2516be20296ce07519d678
jnieh1990 <jnieh1990@gmail.com>
2015-05-26 21:59:36 +0100

    Signed-off-by: jnieh1990 <jnieh1990@gmail.com>

--- test/collections/posts.js
+++ test/collections/posts.js
@@ -3,7 +3,7 @@ Posts = new Mongo.Collection("posts");
 Posts.allow({
     insert: function(userId, doc){
 		
-		if(doc.author !== "Bibi"){
+		if(doc.author !== "Youssef"){
 			throw new Meteor.Error(403, "Vous n'avez pas l'autorisation d'insérer un nouveau post !");
 		}
 		
